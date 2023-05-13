6e0de2dc9b186d46be040ef7658f3e2611be154b
da-penguin-guy <92557504+da-penguin-guy@users.noreply.github.com>
2023-05-13 17:07:57 -0500

    Animations Half Added

--- GameJam2d/Assets/PlayerController.cs
+++ GameJam2d/Assets/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using InputTools;
+using System.Runtime;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,6 +12,14 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
     public float maxSpeed;
+    SpriteAnimator animator;
+    Vector3 originalVectorScale;
+
+    private void Awake()
+    {
+        animator = GetComponent<SpriteAnimator>();
+        originalVectorScale = transform.localScale;
+    }
 
     private void OnEnable()
     {
@@ -27,5 +36,19 @@ public class PlayerController : MonoBehaviour
 
         rb.velocity += rawInput;
         rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), Mathf.Clamp(rb.velocity.y, -maxSpeed, maxSpeed));
+        
+        if (rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-originalVectorScale.x, originalVectorScale.y, originalVectorScale.z);
+        }
+        else if (rb.velocity.x > 0)
+        {
+            transform.localScale = originalVectorScale;
+        }
+        if(Mathf.Abs(rawInput.x + rawInput.y) > 0)
+        {
+            animator.PlayAnim(AnimatorState.Moving);
+        }
+        animator.UpdateSprite();
     }
 }
--- GameJam2d/Assets/SpriteAnimator.cs
+++ GameJam2d/Assets/SpriteAnimator.cs
@@ -1,18 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+using UnityEngine.Playables;
 
 public class SpriteAnimator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite[] idle, moving, shooting, reloading, talking, dying;
+    public AnimatorState currentState;
+    public AnimatorState tempState;
+    public SpriteRenderer renderer;
+    public int currentSprite;
+    public float timer;
+    public float frameRate;
+    public void Init()
     {
-        
+        currentSprite = 0;
+        renderer.sprite = GetCurrentAnims()[currentSprite];
     }
-
-    // Update is called once per frame
-    void Update()
+    public void PlayAnim(AnimatorState state)
     {
-        
+        tempState = state;
     }
+    public void PlayAnimWait(AnimatorState state)
+    {
+        currentState = state; 
+    }
+    public void UpdateSprite()
+    {
+        timer += Time.deltaTime;
+        if (timer > 1/frameRate)
+        {
+            currentSprite = (currentSprite + 1) % GetCurrentAnims().Length;
+            renderer.sprite = GetCurrentAnims()[currentSprite];
+            timer -= 1/frameRate;
+            if(currentSprite == 0)
+            {
+                OnAnimFinished();
+            }
+        }
+    }
+    public void OnAnimFinished()
+    {
+        tempState = currentState;
+    }
+    Sprite[] GetCurrentAnims()
+    {
+        if(tempState == AnimatorState.Idle)
+        {
+            return idle;
+        }
+        if(tempState == AnimatorState.Moving)
+        {
+            return moving;
+        }
+        if(tempState == AnimatorState.Shooting)
+        {
+            return shooting;
+        }
+        if(tempState == AnimatorState.Reloading)
+        {
+            return reloading;
+        }
+        if(tempState == AnimatorState.Talking)
+        {
+            return talking;
+        }
+        if(tempState == AnimatorState.Dying)
+        {
+            return dying;
+        }
+        return idle;
+    }
+}
+public enum AnimatorState
+{
+    Idle,Moving,Shooting,Reloading,Talking,Dying
 }
